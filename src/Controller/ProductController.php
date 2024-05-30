a869b549b434eb01395f9c0335b2b77ce9acc817
damdam48 <damdam.formation@gmail.com>
2024-05-30 12:04:12 +0200

    delete product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -21,15 +21,16 @@ class ProductController extends AbstractController
     ) {
     }
 
-
+    // index
     #[Route('', name: '.index', methods: ['GET'])]
     public function index(ProductRepository $procductRepo): Response
     {
         return $this->render('Backend/Product/index.html.twig', [
-            'product' => $procductRepo->findAll(),
+            'products' => $procductRepo->findAll(),
         ]);
     }
 
+    //create product
     #[Route('/create', name: '.create', methods: ['GET', 'POST'])]
     public function create(Request $request): Response|RedirectResponse
     {
@@ -52,5 +53,49 @@ class ProductController extends AbstractController
         return $this->render('Backend/Product/create.html.twig', [
             'form' => $form,
         ]);
+    }
+
+    //update product
+    #[Route('/{id}/update', name: '.update', methods: ['GET', 'POST'])]
+    public function update(Request $request, ?Product $product): Response|RedirectResponse
+    {
+
+        if (!$product) {
+            $this->addFlash('error', 'product non trouvé');
+
+            return $this->redirectToRoute('admin.product.index');
+        }
+
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->flush();
+
+            $this->addFlash('success', 'Product modifié avec succès');
+
+            return $this->redirectToRoute('admin.product.index');
+        }
+
+        return $this->render('Backend/Product/update.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    //delete product
+    #[Route('/{id}/delete', name: '.delete', methods: ['POST'])]
+    public function deleteProduct(Product $product, Request $request)
+    {
+        if ($this->isCsrfTokenValid('delete' . $product->getId(), $request->request->get('_token'))) {
+            $this->em->remove($product);
+            $this->em->flush();
+
+            $this->addFlash('success', 'Product supprimé avec succès');
+        } else {
+            $this->addFlash('error', 'Token invalide');
+        }
+
+        return $this->redirectToRoute('admin.product.index');
     }
 }
